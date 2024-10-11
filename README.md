9e530e9c5d80da0c6ed6a4a4f57064e5065250f1
Folyd <lyshuhow@gmail.com>
2024-10-11 18:03:03 +0800

    Integrate and benchmark testings

--- README.md
+++ README.md
@@ -1,2 +1,21 @@
-# lox
+# lox-lang
+
 Lox language interpreter written in Rust
+
+## Run
+
+```kt
+fun main() {
+    print "hello world!"
+}
+
+main();
+```
+
+`cargo run example.lox`
+
+## Test
+
+`cargo test`
+
+> Most of the test and benchmark files are copied from [Crafting Interpreters](https://github.com/munificent/craftinginterpreters) repository. The copyright for these belongs to Bob Nystrom and are copied here because their license allows it (MIT).
---
+++ run_lox_benchmarks.py
@@ -0,0 +1,29 @@
+#!/bin/python3
+
+import argparse
+import pathlib
+import subprocess
+import time
+
+BENCHMARK_DIR = "tests/benchmarks/lox"
+
+
+# parser = argparse.ArgumentParser(description="Run Lox benchmarks")
+# parser.add_argument(
+#     "lox_path",
+#     metavar="lox-path",
+#     type=str,
+#     help="Path to the Lox interpreter binary",
+#     default="target/release/lox-lang",
+# )
+# args = parser.parse_args()
+# lox = pathlib.Path(args.lox_path).resolve()
+
+lox_path = "target/release/lox-lang"
+for benchmark in sorted(pathlib.Path(BENCHMARK_DIR).glob("*.lox")):
+    times = []
+    for i in range(5):
+        output = subprocess.check_output([str(lox_path), str(benchmark.resolve())])
+        times.append(float(output.splitlines()[-1]))
+        time.sleep(2)
+    print(f"{benchmark.name}: {round(min(times), 4)}")
